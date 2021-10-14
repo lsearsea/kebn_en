38a64162f9f663ccda4ca077c6b6b06c27c61ce4
holzora <holzora@foxmail.com>
2021-10-14 19:22:08 +0800

    update

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var vm = new Vue({
   el: '.textbook',
   data:{
       Primary:'<li><span class="a" id="a1">小学课本</span><ul class="b" id="b1"><li>语文</li><li>数学</li><li>英语</li></ul></li>',
-      Junior:'<li><span class="a" id="a2">初中课本</span><ul class="b" id="b2"><li>语文</li><li>数学</li><li>英语</li><li>物理</li><li>化学</li><li>道德与法治</li><li>历史</li><li>地理</li><li>生物</li><li>体育</li><li>音乐</li><li>美术</li><li>信息技术</li></ul>',
+      Junior:'<li><span class="a" id="a2">初中课本</span><ul class="b" id="b2"><li>语文</li><li>数学</li><li>英语</li><li>物理</li><li>化学</li><li>政治</li><li>历史</li><li>地理</li><li>生物</li></ul>',
       Senior:'<li><span class="a" id="a3">高中课本</span><ul class="b" id="b3"><li>语文</li><li>数学</li><li>英语</li></ul></li>',
       University:'<li><span class="a" id="a4">大学课本</span><ul class="b" id="b4"><li>文学系</li><li>理学系</li><li>社科系</li></ul></li>'
   }
