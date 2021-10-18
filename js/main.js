3333e526494f282aefba572974a086fd4500ed31
holzora <holzora@foxmail.com>
2021-10-18 23:44:20 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://stats.holzora.com/kebn/cn/stats.php";
+  hm.src = "https://stats.holzora.com/kebn/en/stats.php";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
